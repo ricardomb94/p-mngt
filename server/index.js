4f29855de3a080cb1828b2db78162c903210e0ed
Ricardomb94 <aymardmb@gmail.com>
2022-08-23 00:37:03 +0200

    Connect to mongoDB

--- server/index.js
+++ server/index.js
@@ -1,16 +1,20 @@
 const express = require("express");
+const colors = require("colors");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const connectDB = require("./config/db");
+const port = process.env.PORT || 5000;
+
 const app = express();
 
-const port = process.env.PORT || 5000;
+//Connected to database
+connectDB();
 
 app.use(
   "/graphql",
   graphqlHTTP({
     schema,
-    // rootValue: require("./graphql/resolvers"),
     graphiql: process.env.NODE_ENV === "development",
   })
 );
